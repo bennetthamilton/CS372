403bf137dbd03ac7c821d8e3a0508bcbd4a883b6
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-07 11:16:10 -0700

    initialize socket

---
+++ Projects/Project1/webclient.py
@@ -0,0 +1,16 @@
+# Title: Project 1 - Client
+# Name: Bennett Hamilton
+# Date: 10/7/23
+# Description: prectice setting up a web client on python
+
+# imports
+import socket
+
+# ask OS for socket, assign to variable
+s = socket.socket()
+
+# skip DNS lookup, connect the socket
+
+# send and recieve data
+
+# close connection
