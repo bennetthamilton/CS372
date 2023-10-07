cdb569bd4de8b7db5cc0ebbb467865c1954ce0be
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-07 11:19:11 -0700

    call connect function, call close function

--- Projects/Project1/webclient.py
+++ Projects/Project1/webclient.py
@@ -6,11 +6,17 @@
 # imports
 import socket
 
+# init variiables
+website = "example.com"
+port = 80
+
 # ask OS for socket, assign to variable
 s = socket.socket()
 
-# skip DNS lookup, connect the socket
+# connect the socket (skip DNS lookup)
+s.connect(website, port)
 
 # send and recieve data
 
 # close connection
+s.close()
