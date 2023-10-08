610378e09097928ce4d0eb7a8bd82ad0b5e61b9a
bennetthamilton <114451107+bennetthamilton@users.noreply.github.com>
2023-10-08 11:26:12 -0700

    added try and except for error handling and a way to stop the server while running

--- Projects/Project1/webserver.py
+++ Projects/Project1/webserver.py
@@ -13,48 +13,57 @@ def run_server_response(port):
     # get socket
     s = socket.socket()
 
-    # bind socket
-    s.bind(('', port))
+    try:
+        # bind socket
+        s.bind(('', port))
 
-    # listen for connection
-    s.listen()
-    print(f"Project 1 server listening on port {port}...")
+        # listen for connection
+        s.listen()
+        print(f"Project 1 server listening on port {port}...")
 
-    # accept new connection (continously run loop)
-    while True:
-        
-        new_conn = s.accept()       # accept new connection (tuple)
-        new_socket = new_conn[0]    # assign to new socket
-        print(f"Accepted connection")
+        # accept new connection (continously run loop)
+        while True:
+            
+            new_conn = s.accept()       # accept new connection (tuple)
+            new_socket = new_conn[0]    # assign to new socket
+            print(f"Accepted connection")
 
-        # receive request data from connected client
-        request = b""               # create and empty byte string to append request data
-        while True:                 # continously receive data until there is none left
-            data = new_socket.recv(1024)
-            # break loop when there is not data
-            if not data:
-                break
-            request += data         # append data to request
-            # if blank line delimiting the end of the header detected, break loop
-            if b"\r\n\r\n" in request:
-                break
+            # receive request data from connected client
+            request = b""               # create and empty byte string to append request data
+            while True:                 # continously receive data until there is none left
+                data = new_socket.recv(1024)
+                # break loop when there is not data
+                if not data:
+                    break
+                request += data         # append data to request
+                # if blank line delimiting the end of the header detected, break loop
+                if b"\r\n\r\n" in request:
+                    break
 
-        # build response
-        response = (
-            b"HTTP/1.1 200 OK\r\n"
-            b"Content-Type: text/plain\r\n"
-            b"Content-Length: 6\r\n"
-            b"Connection: close\r\n\r\n"
-            b"Hello!"
-        )
+            # build response
+            response = (
+                b"HTTP/1.1 200 OK\r\n"
+                b"Content-Type: text/plain\r\n"
+                b"Content-Length: 6\r\n"
+                b"Connection: close\r\n\r\n"
+                b"Hello!"
+            )
 
-        # send response
-        new_socket.sendall(response)
+            # send response
+            new_socket.sendall(response)
 
-        # close new socket
-        new_socket.close()
+            # close new socket
+            new_socket.close()
 
-        # loop back to accept new connection
+            # loop back to accept new connection
+
+    except KeyboardInterrupt:
+        print("\nServer stopped")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # close listening socket
+        s.close()
     
 
 # define constants
