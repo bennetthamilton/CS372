9b30a3cf854fad61db4a28e78573106a3e79341d
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-08 10:43:57 -0700

    set up server function

--- Projects/Project1/webserver.py
+++ Projects/Project1/webserver.py
@@ -7,8 +7,34 @@
 import sys
 import socket
 
-# get socket
-s = socket.socket()
+# main server function, send response after receiving request from client
+def run_server_response(port):
+
+    # get socket
+    s = socket.socket()
+
+    # bind socket
+
+
+    # listen for connection
+
+
+    # accept new connection
+
+
+    # receive request from client
+
+
+    # build response
+
+
+    # send response
+
+
+    # close new socket
+
+
+    # loop back to accept new connection
 
 # define constants
 ARG_LEN = 2             # length of arguments
@@ -17,9 +43,10 @@ PORT_DEFAULT = 28333    # default port number
 # assigning port number
 if len(sys.argv) < ARG_LEN:
     # default to example.com 
-    port = PORT_DEFAULT
+    port_number = PORT_DEFAULT
 else:
     # otherwise use given website address
     website = sys.argv[1]
 
 # call function
+run_server_response(port_number)
