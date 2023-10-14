2315248665d1bdf82e06511a1899c5616d4532fb
bennetthamilton <114451107+bennetthamilton@users.noreply.github.com>
2023-10-14 10:23:48 -0700

    seperated into distinct functions for easier legibility of tasks

--- Projects/Project2/main.py
+++ Projects/Project2/main.py
@@ -9,6 +9,45 @@
 import sys
 import socket
 
+# function to receive request data from connected client, returns request as a byte string
+def receive_request(client_socket):
+    request_bstring = b""               # create and empty byte string to append request data
+    while True:                 # continously receive data until there is none left
+        data = client_socket.recv(1024)
+        # break loop when there is not data
+        if not data:
+            break
+        request_bstring += data         # append data to request
+        # if blank line delimiting the end of the header detected, break loop
+        if b"\r\n\r\n" in request_bstring:
+            break
+
+    return request_bstring
+
+# handler function for processing client request
+def process_client_request(client_socket):
+    # receive request data from connected client
+    request = receive_request(client_socket)
+
+    # TODO parse request header to get filename
+
+    # TODO strip off path (for security reasons)
+
+    # TODO read data from filename
+
+    # TODO determine content type (html/txt)
+
+    # TODO build response from txt/html file
+    response = ''
+
+    # send response
+    client_socket.sendall(response)
+
+    # close new socket
+    client_socket.close()
+
+    # loop back to accept new connection in run function
+
 # main server function, send response after receiving request from client
 def run_server_response(port):
 
@@ -29,36 +68,9 @@ def run_server_response(port):
         new_socket = new_conn[0]    # assign to new socket
         print(f"Accepted connection")
 
-        # receive request data from connected client
-        request = b""               # create and empty byte string to append request data
-        while True:                 # continously receive data until there is none left
-            data = new_socket.recv(1024)
-            # break loop when there is not data
-            if not data:
-                break
-            request += data         # append data to request
-            # if blank line delimiting the end of the header detected, break loop
-            if b"\r\n\r\n" in request:
-                break
-
-        # TODO parse request header to get filename
-
-        # TODO strip off path (for security reasons)
-
-        # TODO read data from filename
-
-        # TODO determine content type (html/txt)
-
-        # TODO build response from txt/html file
-        response = ''
-
-        # send response
-        new_socket.sendall(response)
-
-        # close new socket
-        new_socket.close()
-
-        # loop back to accept new connection
+        # receive request data from connected client, get file, and respond with file contents
+        process_client_request(new_socket)
+        
     
 
 # define constants
