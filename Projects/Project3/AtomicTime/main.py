9631930d29db52281979ef4357f716946455464c
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-23 13:10:02 -0700

    initial planning

--- Projects/Project3/AtomicTime/main.py
+++ Projects/Project3/AtomicTime/main.py
@@ -3,3 +3,37 @@
 # Date: 10/21/23
 # Description: reach out to the atomic clock at NIST (National Institute of Standards and Technology) 
 #              and get the number of seconds since January 1, 1900 from their clocks.
+
+# ref: https://beej.us/guide/bgnet0/html/split/project-atomic-time.html
+
+import sys
+import time
+
+def main():
+    # connect to nst server
+
+    # receive 4 bytes of time data
+
+    # decode bytes
+
+    # print out nst time value
+
+    # print out system time value
+
+def system_seconds_since_1900():
+    """
+    The time server returns the number of seconds since 1900, but Unix
+    systems return the number of seconds since 1970. This function
+    computes the number of seconds since 1900 on the system.
+    """
+
+    # Number of seconds between 1900-01-01 and 1970-01-01
+    seconds_delta = 2208988800
+
+    seconds_since_unix_epoch = int(time.time())
+    seconds_since_1900_epoch = seconds_since_unix_epoch + seconds_delta
+
+    return seconds_since_1900_epoch
+
+if __name__ == "__main__":
+    main()
