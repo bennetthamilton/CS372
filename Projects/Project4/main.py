2ed61535351ba930f36373be6b593eff1b36b556
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-29 11:06:53 -0700

    working through functionality of validator function, read both files, assign source and destination ip addrs from txt file, assign data and length from dat file

--- Projects/Project4/main.py
+++ Projects/Project4/main.py
@@ -5,9 +5,6 @@
 # Reference: https://beej.us/guide/bgnet0/html/split/project-validating-a-tcp-packet.html
 
 
-# Read the tcp_addrs_0.txt file.
-
-# Split the line in two, the source and destination addresses.
 
 # function that converts the dots-and-numbers IP addresses into bytestrings
 # ref: https://www.geeksforgeeks.org/python-map-function/
@@ -17,16 +14,28 @@ def ip_to_bytes(ip):
     ip_parts = map(int, ip.split('.'))
     return bytes(ip_parts)      # using bytes() instead of .to_bytes since its an iterable
 
-# Read the tcp_data_0.dat file.
+# function that validates the tcp packet data, return PASS or FAIL
+# ref: https://www.w3schools.com/python/ref_file_readline.asp
+def validate_tcp_packet(ip_filename, tcp_filename):
+    # read the ip_addrs file
+    with open(ip_filename, 'r') as ip_file:
+        # split the line in two, the source and destination addresses
+        source_ip, dest_ip = ip_file.readline().split(' ')
+        # function that generates the IP pseudo header bytes from the IP addresses from tcp_addrs_0.txt
 
-# function that generates the IP pseudo header bytes from the IP addresses from tcp_addrs_0.txt and the TCP length from the tcp_data_0.dat file.
+    # read the tcp_data file
+    with open(tcp_filename, 'rb') as tcp_file:
+        tcp_data = tcp_file.read()
+        tcp_length = len(tcp_data)
+        # function that generates the TCP length from the tcp_data_0.dat file.
+    
 
-# Build a new version of the TCP data that has the checksum set to zero.
+    # Build a new version of the TCP data that has the checksum set to zero.
 
-# Concatenate the pseudo header and the TCP data with zero checksum.
+    # Concatenate the pseudo header and the TCP data with zero checksum.
 
-# Compute the checksum of that concatenation
+    # Compute the checksum of that concatenation
 
-# Extract the checksum from the original data in tcp_data_0.dat.
+    # Extract the checksum from the original data in tcp_data_0.dat.
 
-# Compare the two checksums
+    # Compare the two checksums
