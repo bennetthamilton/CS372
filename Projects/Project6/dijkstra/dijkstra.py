4fd4f1a8991c0cc230281fcf41ac1ec34af2d145
bennetthamilton <bennetthamilton0@gmail.com>
2023-11-11 10:46:57 -0800

    added functions from project 5

--- Projects/Project6/dijkstra/dijkstra.py
+++ Projects/Project6/dijkstra/dijkstra.py
@@ -65,6 +65,150 @@ def dijkstras_shortest_path(routers, src_ip, dest_ip):
     # TODO Write me!
     pass
 
+# ref: Project 5 from netfuncs.py
+def find_router_for_ip(routers, ip):
+    """
+    Search a dictionary of routers (keyed by router IP) to find which
+    router belongs to the same subnet as the given IP.
+
+    Return None if no routers is on the same subnet as the given IP.
+
+    FOR FULL CREDIT: you must do this by calling your ips_same_subnet()
+    function.
+
+    Example:
+
+    [Note there will be more data in the routers dictionary than is
+    shown here--it can be ignored for this function.]
+
+    routers: {
+        "1.2.3.1": {
+            "netmask": "/24"
+        },
+        "1.2.4.1": {
+            "netmask": "/24"
+        }
+    }
+    ip: "1.2.3.5"
+    return: "1.2.3.1"
+
+
+    routers: {
+        "1.2.3.1": {
+            "netmask": "/24"
+        },
+        "1.2.4.1": {
+            "netmask": "/24"
+        }
+    }
+    ip: "1.2.5.6"
+    return: None
+    """
+
+    # ref: https://www.programiz.com/python-programming/methods/dictionary/items
+    for router_ip, router_info in routers.items():
+        netmask_slash = router_info.get("netmask")
+        # if there is a subnet match then return that ipv4 address
+        if ips_same_subnet(router_ip, ip, netmask_slash):
+            return router_ip
+
+    return None    # if there are no routers with matching subnet
+
+# ref: Project 5 from netfuncs.py
+def ips_same_subnet(ip1, ip2, slash):
+    """
+    Given two dots-and-numbers IP addresses and a subnet mask in slash
+    notataion, return true if the two IP addresses are on the same
+    subnet.
+
+    Returns a boolean.
+
+    FOR FULL CREDIT: this must use your get_subnet_mask_value() and
+    ipv4_to_value() functions. Don't do it with pure string
+    manipulation.
+
+    This needs to work with any subnet from /1 to /31
+
+    Example:
+
+    ip1:    "10.23.121.17"
+    ip2:    "10.23.121.225"
+    slash:  "/23"
+    return: True
+    
+    ip1:    "10.23.230.22"
+    ip2:    "10.24.121.225"
+    slash:  "/16"
+    return: False
+    """
+
+    # ref: https://beej.us/guide/bgnet0/html/split/ip-subnets-and-subnet-masks.html
+    subnet_mask = get_subnet_mask_value(slash)
+    
+    # convert ipv4 addresses to 32 bit integer value
+    ip1_value = ipv4_to_value(ip1)
+    ip2_value = ipv4_to_value(ip2)
+
+    # use mask to get subnets from ipv4 addresses
+    ip1_subnet = ip1_value & subnet_mask
+    ip2_subnet = ip2_value & subnet_mask
+
+    # compare, return true or false
+    return ip1_subnet == ip2_subnet
+
+# ref: Project 5 from netfuncs.py
+def get_subnet_mask_value(slash):
+    """
+    Given a subnet mask in slash notation, return the value of the mask
+    as a single number of integer type. The input can contain an IP
+    address optionally, but that part should be discarded.
+
+    Returns an integer type.
+
+    Example:
+
+    There is only one return value, but it is shown here in 3 bases.
+
+    slash:  "/16"
+    return: 0xffff0000 0b11111111111111110000000000000000 4294901760
+
+    slash:  "10.20.30.40/23"
+    return: 0xfffffe00 0b11111111111111111111111000000000 4294966784
+    """
+    TOTAL_BITS = 32
+    # get only the slash and following number
+    length = int(slash.split("/")[1])
+
+    # calculate mask value using prefix length
+    mask_value = (0xffffffff << (TOTAL_BITS - length)) & 0xffffffff
+
+    return mask_value
+
+# ref: Project 5 from netfuncs.py
+def ipv4_to_value(ipv4_addr):
+    """
+    Convert a dots-and-numbers IP address to a single 32-bit numeric
+    value of integer type. Returns an integer type.
+
+    Example:
+
+    ipv4_addr: "255.255.0.0"
+    return:    4294901760  (Which is 0xffff0000 hex)
+
+    ipv4_addr: "1.2.3.4"
+    return:    16909060  (Which is 0x01020304 hex)
+    """
+
+    ipv4_addr_bytes = ipv4_addr.split(".")
+
+    # no need to convert to hex since its the same value, therefore bitwise function is the same
+    # ref: https://beej.us/guide/bgnet0/html/split/ip-subnets-and-subnet-masks.html
+    ipv4_value = 0
+    for part in ipv4_addr_bytes:
+        ipv4_value = (ipv4_value << 8) | int(part)
+
+    return ipv4_value
+
 #------------------------------
 # DO NOT MODIFY BELOW THIS LINE
 #------------------------------
