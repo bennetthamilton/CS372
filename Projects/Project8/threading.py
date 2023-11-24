6098706fa03e256e126ecdf028c154043b883ab6
bennetthamilton <bennetthamilton0@gmail.com>
2023-11-24 09:02:57 -0800

    initial commit for project 8 threading

---
+++ Projects/Project8/threading.py
@@ -0,0 +1,6 @@
+# Title: Project 8 - Using Threading
+# Name: Bennett Hamilton
+# Date: 11/24/23
+# Description: receive multiple ranges of numbers, add numbers in each range together, 
+#              print total sums, print sum of the total sums
+
