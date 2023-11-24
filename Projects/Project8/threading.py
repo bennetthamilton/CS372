2177fe592f09e780ffc1d6b7acfc5d7136bb721a
bennetthamilton <bennetthamilton0@gmail.com>
2023-11-24 09:09:52 -0800

    planning out code

--- Projects/Project8/threading.py
+++ Projects/Project8/threading.py
@@ -4,3 +4,29 @@
 # Description: receive multiple ranges of numbers, add numbers in each range together, 
 #              print total sums, print sum of the total sums
 
+import threading
+
+def sum_range():
+    # function to add up a range of numbers
+    pass
+
+def main():
+    # example input
+    ranges = [
+        [10, 20],
+        [1, 5],
+        [70, 80],
+        [27, 92],
+        [0, 16]
+    ]
+
+    # launch threads per each range
+        # create new thread that runs sum_range
+        # append to existing threads
+        # start running
+
+    # close threads (join)
+
+    # print results
+
+    pass
