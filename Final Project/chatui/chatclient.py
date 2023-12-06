8a8196c5ecf69869c00315bf71d7dff319f4a36a
bennetthamilton <bennetthamilton0@gmail.com>
2023-12-06 11:18:08 -0800

    init commit for final project, added files and headers

---
+++ Final Project/chatui/chatclient.py
@@ -0,0 +1,5 @@
+# Title: Final Project - Chat Client
+# Name: Bennett Hamilton
+# Date: 12/6/23
+# Description: implementation of a chat client as specified by Project: Multiuser Chat Client and Server
+#              ref: https://beej.us/guide/bgnet0/html/split/project-multiuser-chat-client-and-server.html
