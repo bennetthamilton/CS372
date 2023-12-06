afbb43d3a2b2049e7ed36a7a26a0f07f024d80bc
bennetthamilton <bennetthamilton0@gmail.com>
2023-12-06 12:18:57 -0800

    added initial function outline for chat client

--- Final Project/chatui/chatclient.py
+++ Final Project/chatui/chatclient.py
@@ -4,3 +4,25 @@
 # Description: implementation of a chat client as specified by Project: Multiuser Chat Client and Server
 #              ref: https://beej.us/guide/bgnet0/html/split/project-multiuser-chat-client-and-server.html
 
+import socket
+import threading
+import json
+from chatui import init_windows, read_command, print_message, end_windows
+
+def run_client(nick, server_address, server_port):
+    pass
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: python chat_client.py <nick> <server_address> <server_port>")
+        sys.exit(1)
+
+    nick = sys.argv[1]
+    server_address = sys.argv[2]
+    server_port = int(sys.argv[3])
+
+    init_windows()
+    run_client(nick, server_address, server_port)
+    end_windows()
